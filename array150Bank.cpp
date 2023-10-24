7de39a86758e6ecdecc35e3aa8b9a98cd8b71bbb
int33k <126438282+int33k@users.noreply.github.com>
2023-10-25 03:14:51 +0530

    Update array150Bank.cpp

--- array150Bank.cpp
+++ array150Bank.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <filesystem>
 #include <string>
-#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -59,76 +57,54 @@ void PrintingPress(RECORDS *arr,fstream &logfile, int i){
 }
 
 
-// not to be in the class if so
-
-/* int customStringToInt(const string& str) {
-    int result = 0;
-
-    for (size_t i = 0; i < str.size(); ++i) {
-            result = result * 10 + (str[i] - '0');
-        }
-
-    return result;
-} */
-
-
-
 int main(){
     system("clear");
 
     fstream logfile("customer_data.txt",ios::in | ios::out | ios::app);
-    //FILE *logfile = fopen("customer_data.txt","a+");
-
-    char boool;
+    
+    string buffer;
     const int numClient = 150;
     RECORDS *arr[numClient];
-
-
+  
     for(int i = 0; i < numClient; i++) arr[i] = mallocing();
 
+    for(size_t i = 0; i < numClient ; i++){
 
-     for(size_t i = 0; i < numClient ; i++){
+      if(i==0){
+          
+            logfile.seekg(0, ios::end);
+            streampos fileSize = logfile.tellg(); 
+            logfile.seekg(0, ios::beg);
 
-        /*if(i==0){
-
-            logfile.seekg(-1, ios_base::end);
-            char ch;
-            logfile.get(ch);
-
-             if(ch=='\0') 
-            {
+             if(fileSize == 0){
                 logfile << "\t\t\tCLIENTS'S DATA : " << endl;
                 logfile << "\t\t\t---------------" << endl;
-            } 
-            else
-            {
-                logfile.seekg(-3, ios_base::end);
-                char buffer[3];
-                logfile.read(buffer,3);
-                string str(buffer);
-                int num = stoi(str);
-                //i = customStringToInt(str); 
-                i = num;
-            }  
-            
-        }  */
- 
-        if(i==20) break;
+              } 
+            else{   
+                    logfile.seekg(static_cast<int>(fileSize) - 3);
+                    char character;
+                    while (logfile.get(character)) {
+                        buffer.push_back(character);
+                    }
+                    i = stoi(buffer);
+                    logfile.seekg(0, ios::end); 
+                }
 
-        Credentials_Appending(arr[i],"rajat","kurla",15000,MEDIUM);
+          }
 
-        PrintingPress(arr[i],logfile,i);
-    }
+      if(i==20) break; //checking for case one with this line uncommented and later commented to check else part is working or not so that to get file entry from index 21
+       
+      Credentials_Appending(arr[i],"rajat","kurla",15000+100*i,MEDIUM);
+  
+      PrintingPress(arr[i],logfile,i);
+       
+     }
 
     logfile.close();   
-
-    cout << arr[10]->name << endl << arr[10]->clientstatus << endl << endl;
-    cout << arr[40]->name << endl << arr[40]->clientstatus << endl << endl;
-    
+  
     for(size_t i = 0; i < numClient ; i++){
         delete arr[i];
     }
+
+  return 0;
 }
-
-
-
